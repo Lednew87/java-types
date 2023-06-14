aac8d3c4b80e12b30a85672f688bffbd69f52943
Wendel Tonzar <wendeltonzar@gmail.com>
2023-06-14 19:59:27 -0300

    Types Data in Java.

---
+++ src/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte small = -128;
+        short large = 32;
+        int number = 255;
+        float pi = 3.14f;
+        double money = 25.656;
+        long id = 1065L;
+        char option = 65;
+        boolean license = true;
+        String name = "Lednewcodes";
+
+
+        System.out.println("Byte value: " + small);
+        System.out.println("Short value: " + large);
+        System.out.println("Int value: " + number);
+        System.out.println("Long value: " + id);
+        System.out.println("Float value: " + pi);
+        System.out.println("Double value: " + money);
+        System.out.println("Character value: " + option);
+        System.out.println("Boolean value: " + license);
+        System.out.println("String value: " + name);
+    }
+}
+
